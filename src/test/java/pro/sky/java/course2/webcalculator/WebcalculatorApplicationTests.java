c4699e45745a5c1fe3abfed126077e9a7690a3ab
Romchig <romulusto@gmail.com>
2023-08-15 18:25:50 +0200

    Почти готово

---
+++ src/test/java/pro/sky/java/course2/webcalculator/WebcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.webcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
